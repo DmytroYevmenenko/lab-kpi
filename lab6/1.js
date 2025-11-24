f434b0e84aac0cb794fee142a8650557a4ddf688
Dmytro Yevmenenko <evmenenkooor@gmail.com>
2025-11-24 17:53:49 +0200

    6

---
+++ lab6/1.js
@@ -0,0 +1,25 @@
+const pipe = (...fns) => {
+    for (let i = 0; i < fns.length; i++) {
+        if (typeof fns[i] !== 'function') {
+            throw new Error('Error');
+        }
+    }
+    
+    return (x) => fns.reduce((acc, fn) => fn(acc), x);
+};
+const inc = x => ++x;
+const twice = x => x * 2;
+const cube = x => x ** 3;
+
+const f1 = pipe(inc, twice, cube);
+console.log(f1(5));
+
+const f2 = pipe(inc, inc);
+console.log(f2(7));
+
+try {
+  const f3 = pipe(inc, 7, cube);
+  console.log(f3(10));
+} catch (error) {
+  console.error('Error:', error.message);
+}
---
+++ lab6/2.js
@@ -0,0 +1,55 @@
+const composeSafe = (...fns) => {
+  if (!fns.every(fn => typeof fn === 'function')) {
+    throw new TypeError('All arguments must be functions');
+  }
+
+  const errorCallbacks = [];
+
+  const composed = (x) => {
+    let current = x;
+    for (let i = fns.length - 1; i >= 0; i--) {
+      try {
+        current = fns[i](current);
+      } catch (err) {
+        errorCallbacks.forEach(cb => cb(err));
+        return undefined;
+      }
+    }
+    return current;
+  };
+
+  composed.on = (event, callback) => {
+    if (event === 'error' && typeof callback === 'function') {
+      errorCallbacks.push(callback);
+    }
+    return composed;
+  };
+
+  composed.off = (event, callback) => {
+    if (event === 'error') {
+      const index = errorCallbacks.indexOf(callback);
+      if (index !== -1) {
+        errorCallbacks.splice(index, 1);
+      }
+    }
+    return composed;
+  };
+
+  return composed;
+};
+
+const inc = x => ++x;
+const twice = x => x * 2;
+const cube = x => x ** 3;
+
+const f1 = composeSafe(inc, twice, cube);
+console.log(f1(5));
+
+const f2 = composeSafe(inc, inc);
+console.log(f2(7));
+
+try {
+  const h = composeSafe(inc, 7, cube);
+} catch (error) {
+  console.log(error.message);
+}
