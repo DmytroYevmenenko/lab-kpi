c5b189a3259d910a06e1f710d24c11f1db014585
DmytroYevmenenko <evmenenkooor@gmail.com>
2025-11-25 19:57:03 +0200

    1

---
+++ lab7/1.js
@@ -0,0 +1,8 @@
+const array1 = [1, 2, 3, 4, 5, 6, 7];
+
+const removeElement = (array, item) => {
+  const index = array.indexOf(item);
+  if (index !== -1) array.splice(index, 1);
+};
+
+console.log(removeElement(array1, 2));
